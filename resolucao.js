09de1ee42021b164ee7727a117bb541f2030dbce
VioletaRS26 <svioleta1002@gmail.com>
2023-02-09 20:06:48 -0300

    inicio da resolução

---
+++ resolucao.js
@@ -0,0 +1,8 @@
+//Exercicio 01
+const menor = (a, b, c) => a + b < c ;
+console.log(menor(1,4,6));
+
+//Exercicio 04
+const dobro_triplo = n => (n < 0) ? n * 3  : n * 2; //Operador ternário
+console.log(dobro_triplo(-4));
+
